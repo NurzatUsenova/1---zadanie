4d7c6fc4e16c0efdd0107fb85e192300d1acd35d
nurzat.usenkyzy.kk <88789310+NurzatUsenova@users.noreply.github.com>
2022-01-29 12:09:42 +0300

    Add files via upload

---
+++ 1 - zadanie/3/script.js
@@ -0,0 +1,9 @@
+//3. Напишите несколько строк кода, выводящих на экран ваше имя и почтовый адрес.
+
+let myName = "Nurzat";
+let myIpAdress = "nurzat.usenkyzy.kk@gmail.com";
+
+document.write(` Меня зовут:${myName} <br>`);
+document.write(`Мой почтовый адрес :${myIpAdress}`);
+
+
